c9dfd6006c0a7cc73dacfbb0c1b691b0e81d5cfe
nobushi95 <nobushi1121@gmail.com>
2023-07-21 00:53:38 +0900

    ViewModelを追加してBind

---
+++ ViewModels/MainPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
+using WinUI3_DataTemplate_Bind_ParentProperty.Views.Data;
+
+namespace WinUI3_DataTemplate_Bind_ParentProperty.ViewModels
+{
+    internal partial class MainPageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private List<MainPageItemViewData> _itemsViewData = new()
+        {
+            new("\ue701", "Wifi"),
+            new("\ue702", "Bluetooth"),
+            new("\ue706", "Brightness"),
+            new("\ue709", "Airplane"),
+        };
+    }
+}
--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -1,17 +1,5 @@
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Microsoft.UI.Xaml.Controls;
+using WinUI3_DataTemplate_Bind_ParentProperty.ViewModels;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,6 +11,7 @@ namespace WinUI3_DataTemplate_Bind_ParentProperty.Views
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private readonly MainPageViewModel _viewModel = new();
         public MainPage()
         {
             this.InitializeComponent();
